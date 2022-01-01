d81740f2d5cae8bebc7f8133f7e83000ebb4341f
Thanasis Galatis <gnasos219@gmail.com>
2022-01-02 01:30:27 +0200

    [home page] - many UI changes and css fixes - dark mode finished for this page, also working in other pages too. Remains to be tested

--- src/script.js
+++ src/script.js
@@ -1,26 +1,64 @@
 window.addEventListener("load", () => {
   console.log("dark mode on/off");
-  let checkbox = document.getElementById("ChangeTheme");
 
-  //check storage if dark mode was on or off
+  let checkbox = document.getElementById("ChangeTheme") || null;
+  let darkModeButton = document.getElementById("darkmodeBtn") || null;
+  let languageButton = document.getElementById("langSelectBox") || null;
+  let languageDiv = document.getElementById("languageDiv") || null;
+  let mainHeading = document.getElementById("mainHeading") || null;
+  let minorHeading = document.getElementById("minorHeading") || null;
+  let content = document.getElementById("content") || null;
+  let logo = document.getElementById("uopLogo") || null;
+  let toBeChanged =
+    [checkbox, languageButton, mainHeading, minorHeading] || null;
+
+  // Check storage if dark mode was on or off
   if (sessionStorage.getItem("mode") == "dark") enableDarkMode();
   else noDarkMode();
 
-  //if the checkbox state is changed, act accordingly
+  // If the checkbox state is changed, act accordingly
   checkbox.addEventListener("change", function () {
     if (checkbox.checked) enableDarkMode();
     else noDarkMode();
   });
 
+  darkModeButton?.addEventListener("click", function () {
+    checkbox.checked = !checkbox.checked;
+    if (checkbox.checked) enableDarkMode();
+    else noDarkMode();
+  });
+
   function enableDarkMode() {
-    document.body.classList.add("dark-mode");
-    checkbox.checked = true;
-    sessionStorage.setItem("mode", "dark");
+    try {
+      document.body.classList.add("dark-mode");
+      content.classList.add("dark-mode-body");
+      languageDiv.classList.add("lang-dark");
+      toBeChanged.forEach(function (element) {
+        element.classList.add("dark-mode-text");
+      });
+      logo.src = "assets/images/uop.png";
+
+      checkbox.checked = true;
+      sessionStorage.setItem("mode", "dark");
+    } catch (error) {
+      console.log("error: " + error);
+    }
   }
 
   function noDarkMode() {
-    document.body.classList.remove("dark-mode");
-    checkbox.checked = false;
-    sessionStorage.setItem("mode", "light");
+    try {
+      document.body.classList.remove("dark-mode");
+      content.classList.remove("dark-mode-body");
+      languageDiv.classList.remove("lang-dark");
+      toBeChanged.forEach(function (element) {
+        element.classList.remove("dark-mode-text");
+      });
+      logo.src = "assets/images/v110_3.png";
+
+      checkbox.checked = false;
+      sessionStorage.setItem("mode", "light");
+    } catch (error) {
+      console.log("error: " + error);
+    }
   }
 });
