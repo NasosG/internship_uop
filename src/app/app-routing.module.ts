25955d8c042b357f052005fc220ccc4811ebab98
Thanasis Galatis <gnasos219@gmail.com>
2022-01-04 00:21:27 +0200

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StudentProfileComponent } from './student-profile/student-profile.component';
+import { StudentComponent } from './student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', 
+  component: HomeComponent
+},
+{
+  path: 'student', 
+  component: StudentComponent
+},
+{
+  path: 'profile', 
+  component: StudentProfileComponent
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
