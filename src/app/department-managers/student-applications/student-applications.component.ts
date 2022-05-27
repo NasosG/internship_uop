607fea257b328077a32e2542c1ea28a937a54ec8
Thanos Konstantopoulos <thanos2259@gmail.com>
2022-05-27 15:23:41 +0300

    fields added on department managers application sheet + order changed

--- src/app/department-managers/student-applications/student-applications.component.ts
+++ src/app/department-managers/student-applications/student-applications.component.ts
@@ -1,13 +1,13 @@
 import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
-import {MatDialog, MatDialogRef} from '@angular/material/dialog';
-import {TranslateService} from '@ngx-translate/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { TranslateService } from '@ngx-translate/core';
 import { DataTableDirective } from 'angular-datatables';
-import {Utils} from 'src/app/MiscUtils';
-import {Student} from 'src/app/students/student.model';
+import { Utils } from 'src/app/MiscUtils';
+import { Student } from 'src/app/students/student.model';
 import * as XLSX from 'xlsx';
 
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {DepManagerService} from '../dep-manager.service';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DepManagerService } from '../dep-manager.service';
 
 @Component({
   selector: 'app-student-applications',
@@ -23,7 +23,7 @@ export class StudentApplicationsComponent implements OnInit, AfterViewInit {
   ngSelect = "";
   constructor(public depManagerService: DepManagerService, private chRef: ChangeDetectorRef, private translate: TranslateService, public dialog: MatDialog) { }
 
-  dtOptions : any = {};
+  dtOptions: any = {};
 
   ngOnInit() {
     this.depManagerService.getStudentsApplyPhase()
@@ -32,37 +32,37 @@ export class StudentApplicationsComponent implements OnInit, AfterViewInit {
         for (let i = 0; i < students.length; i++) {
           this.studentsData[i].schacpersonaluniquecode = this.getAM(students[i].schacpersonaluniquecode);
         }
-      // Have to wait till the changeDetection occurs. Then, project data into the HTML template
-      this.chRef.detectChanges();
+        // Have to wait till the changeDetection occurs. Then, project data into the HTML template
+        this.chRef.detectChanges();
 
-      // Use of jQuery DataTables
-      const table: any = $('#example');
-      this.table = table.DataTable({
-        lengthMenu: [
+        // Use of jQuery DataTables
+        const table: any = $('#example');
+        this.table = table.DataTable({
+          lengthMenu: [
             [10, 25, 50, -1],
             [10, 25, 50, 'All']
-        ],
-        lengthChange: true,
-        paging: true,
-        searching: true,
-        ordering: true,
-        info: true,
-        autoWidth: false,
-        responsive: true,
-        select: true,
-        pagingType: 'full_numbers',
-        processing: true,
-        language: {
-          // lengthMenu: 'Show _MENU_ entries'
-          // lengthMenu: this.translate.instant('DEPT-MANAGER.SHOW-RESULTS') + ' _MENU_ ' + this.translate.instant('DEPT-MANAGER.ENTRIES')
+          ],
+          lengthChange: true,
+          paging: true,
+          searching: true,
+          ordering: true,
+          info: true,
+          autoWidth: false,
+          responsive: true,
+          select: true,
+          pagingType: 'full_numbers',
+          processing: true,
+          language: {
+            // lengthMenu: 'Show _MENU_ entries'
+            // lengthMenu: this.translate.instant('DEPT-MANAGER.SHOW-RESULTS') + ' _MENU_ ' + this.translate.instant('DEPT-MANAGER.ENTRIES')
             // : "Επίδειξη","ENTRIES": "εγγραφών ανά σελίδα"
-          // // lengthMenu: 'Display _MENU_ records per page',
-          // zeroRecords: 'Nothing found - sorry',
-          // info: 'Showing page _PAGE_ of _PAGES_',
-          // infoEmpty: 'No records available',
-          // infoFiltered: '(filtered from _MAX_ total records)',
-        },
-        // pageLength: 8
+            // // lengthMenu: 'Display _MENU_ records per page',
+            // zeroRecords: 'Nothing found - sorry',
+            // info: 'Showing page _PAGE_ of _PAGES_',
+            // infoEmpty: 'No records available',
+            // infoFiltered: '(filtered from _MAX_ total records)',
+          },
+          // pageLength: 8
         });
       });
   }
@@ -74,36 +74,39 @@ export class StudentApplicationsComponent implements OnInit, AfterViewInit {
   }
 
   exportToExcel() {
-    let studentsDataJson:any = [];
+    let studentsDataJson: any = [];
     for (const item of this.studentsData) {
       studentsDataJson.push({
         "Επώνυμο": item.sn,
         "Όνομα": item.givenname,
+        "Πατρώνυμο": item.father_name,
+        "Μητρώνυμο": item.mother_name,
+        "Επώνυμο πατέρα": item.father_last_name,
+        "Επώνυμο μητέρας": item.mother_last_name,
+        "Ημ/νια Γέννησης": Utils.reformatDateOfBirth(item.schacdateofbirth),
+        "Έτος γέννησης": item.schacyearofbirth,
         "ΑΜ": item.schacpersonaluniquecode,
         "Φύλο": item.schacgender == 1 ? 'Άνδρας' : 'Γυναίκα',
-        "Έτος γέννησης": item.schacyearofbirth,
-        "Ημ/νια Γέννησης": Utils.reformatDateOfBirth(item.schacdateofbirth),
-        "Πατρώνυμο": item.father_name,
-        "Επώνυμο πατέρα": item.father_last_name,
-        "Μητρώνυμο": item.mother_name,
-        "Επώνυμο μητέρας": item.mother_last_name,
+        "Τηλέφωνο": item.phone,
+        "Πόλη": item.city,
+        "ΤΚ": item.post_address,
+        "Διεύθυνση": item.address,
+        "Τοποθεσία": item.location,
+        "Χώρα": item.country == "gr",
         "ΑΦΜ": item.ssn,
         "ΔΟΥ": item.doy,
         "IBAN": item.iban,
         "Εκπαίδευση": item.education,
+        "Άλλη εκπαίδευση": item.other_edu,
+        "Γνώσεις Η/Υ": item.computer_skills,
+        "skills": item.skills,
+        "honors": item.honors,
         "Εμπειρία": item.experience,
         "Γλώσσες": item.languages,
-        "Γνώσεις Η/Υ": item.computer_skills,
-        "Άλλη εκπαίδευση": item.other_edu,
-        "honors": item.honors,
         "Ενδιαφέροντα": item.interests,
-        "skills": item.skills,
-        "Τηλέφωνο": item.phone,
-        "Διεύθυνση": item.address,
-        "Τοποθεσία": item.location,
-        "Πόλη": item.city,
-        "ΤΚ": item.post_address,
-        "Χώρα": item.country == "gr" ? 'Eλλάδα' : item.country,
+        "Υπηρετώ στο στρατό ": item.military_training == true ? 'ΝΑΙ' : 'ΟΧΙ',
+        "AMEA κατηγορίας 5 ": item.amea_cat == true ? 'ΝΑΙ' : 'ΟΧΙ',
+        "Σύμβαση εργασίας ": item.working_state == true ? 'ΝΑΙ' : 'ΟΧΙ',
         "Αποτελέσματα": (item.phase == 2 ? 'Επιλέχτηκε' : item.phase == 1 ? 'Προς επιλογή' : 'Απορρίφτηκε')
         // "edupersonorgdn": item.edupersonorgdn,
       });
@@ -124,9 +127,9 @@ export class StudentApplicationsComponent implements OnInit, AfterViewInit {
   }
 
   printDataTable() {
-    let currentDate = new Date().toJSON().slice(0,10).split('-').reverse().join('/');
+    let currentDate = new Date().toJSON().slice(0, 10).split('-').reverse().join('/');
     const windowPrint = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
-    windowPrint?.document.write("<h5 style='text-align: right;'>"+ currentDate +"</h5><br>");
+    windowPrint?.document.write("<h5 style='text-align: right;'>" + currentDate + "</h5><br>");
     windowPrint?.document.write("<table style=\"width: 100%;\"> \
         <thead style=\"color:white; background-color:#2d4154;\"> \
           <tr> \
@@ -141,15 +144,15 @@ export class StudentApplicationsComponent implements OnInit, AfterViewInit {
     let i = 0;
     for (let student of this.studentsData) {
       windowPrint?.document.write(
-      // print the rows - another color for the odd lines - could be done with i % 2 != 0
-      // but with bitwise operator it was a bit faster
-        "<tr " + ( (i & 1) ? "style=\"background-color: #f3f3f3;\">" : ">" ) +
-                "<td>" + student.sn + " " + student.givenname + "</td>" +
-                "<td>" + student.father_name + "</td>" +
-                "<td>" + student.schacpersonaluniquecode + "</td>" +
-                "<td>" + student.id + "@uop.gr" + "</td>" +
-                "<td>" + (student.phase == 2 ? 'Επιλέχτηκε' : student.phase == 1 ? 'Προς επιλογή' : 'Απορρίφτηκε') + "</td>" +
-      "</tr>");
+        // print the rows - another color for the odd lines - could be done with i % 2 != 0
+        // but with bitwise operator it was a bit faster
+        "<tr " + ((i & 1) ? "style=\"background-color: #f3f3f3;\">" : ">") +
+        "<td>" + student.sn + " " + student.givenname + "</td>" +
+        "<td>" + student.father_name + "</td>" +
+        "<td>" + student.schacpersonaluniquecode + "</td>" +
+        "<td>" + student.id + "@uop.gr" + "</td>" +
+        "<td>" + (student.phase == 2 ? 'Επιλέχτηκε' : student.phase == 1 ? 'Προς επιλογή' : 'Απορρίφτηκε') + "</td>" +
+        "</tr>");
       i++;
     }
     windowPrint?.document.write("</table>")
@@ -173,11 +176,11 @@ export class StudentApplicationsComponent implements OnInit, AfterViewInit {
 
 
 
-  openDialog(idx:any) {
+  openDialog(idx: any) {
     // console.log(idx);
     const dialogRef = this.dialog.open(StudentAppsPreviewDialog, {
       // width: '350px',
-      data: {studentsData: this.studentsData, index: idx}
+      data: { studentsData: this.studentsData, index: idx }
     });
 
     // dialogRef.afterClosed().subscribe(result => {
