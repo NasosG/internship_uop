396c2725e12fbb11c6a8aec79c6db74bba85f1bc
Thanasis Galatis <gnasos219@gmail.com>
2023-02-21 22:50:43 +0200

    jest test student proc grade added

---
+++ backend/tests/studentGradeProcedure.test.js
@@ -0,0 +1,29 @@
+// database connection configuration
+const mssql = require("../secretariat_db_config.js");
+const msql = require('mssql');
+require('dotenv').config();
+
+describe('getStudentFactorProcedure', () => {
+  it('should return the correct values for a valid department ID and student AM', async () => {
+    const depId = process.env.depId;
+    const studentAM = process.env.studentAm;
+    const result = await getStudentFactorProcedure(depId, studentAM);
+    console.log("res " + result);
+    // expect(result.Praktiki).toBeFalse();
+  });
+});
+
+const getStudentFactorProcedure = async (depId, studentAM) => {
+  try {
+    let mspool = await msql.connect(mssql);
+
+    const result = await mspool.request()
+      .input('DepId', msql.Int, depId)
+      .input('am', msql.VarChar(100), studentAM)
+      .execute('usp_GetStudentFactorPraktiki');
+    console.log(result.recordset[0]);
+    return result.recordset[0];
+  } catch (error) {
+    console.log("error: " + error);
+  }
+};
