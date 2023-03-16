eab16449cdcb443e341a0a701e4f22d4d296b362
NasosG <53830262+NasosG@users.noreply.github.com>
2023-03-16 03:16:25 +0200

    docxtemplater added

---
+++ src/app/generic-components/contract-creation/contract-creation.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-creation',
+  templateUrl: './contract-creation.component.html',
+  styleUrls: ['./contract-creation.component.css']
+})
+export class ContractCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
