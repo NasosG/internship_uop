c91aefb193ede44aff1f6bd40106e6c1698282ce
NasosG <53830262+NasosG@users.noreply.github.com>
2023-05-24 22:25:30 +0300

    change update academics atlas bug

--- backend/services/atlasService.js
+++ backend/services/atlasService.js
@@ -357,14 +357,13 @@ const updatePositionsList = async (data) => {
         item.atlasPositionId
         ]);
 
-      // Insert academics into academics table
-      // for (let academic of item.academics) {
-      //   const result = await pool.query("SELECT * FROM position_has_academics WHERE position_id=$1 AND academic_id=$2", [item.atlasPositionId, academic.academicsId]);
-      //   if (result.rowCount === 0) {
-      //     await pool.query("INSERT INTO position_has_academics(position_id, academic_id)" +
-      //       " VALUES ($1, $2)", [item.atlasPositionId, academic.academicsId]);
-      //   }
-      // }
+      // Delete all previous academics for this position id
+      await pool.query("DELETE FROM position_has_academics WHERE position_id = $1", [item.atlasPositionId]);
+      // Re - Insert academics into academics table
+      for (let academic of item.academics) {
+        await pool.query("INSERT INTO position_has_academics(position_id, academic_id)" +
+          " VALUES ($1, $2)", [item.atlasPositionId, academic.academicsId]);
+      }
 
     }
     // return updateResults;
