05a29558c84c8bec93674f5bf6fa732c4ebb88f8
Thanasis Galatis <gnasos219@gmail.com>
2025-03-30 11:21:02 +0300

    Update atlasController.js

--- backend/controllers/atlasController.js
+++ backend/controllers/atlasController.js
@@ -612,7 +612,7 @@ const insertOrUpdateAtlasTables = async (/*emergency = 0*/) => {
     let availablePositionGroups;
 
     // Get the count of position group pairs of the previous job run (the previous hour)
-    let skip = 0;//await atlasService.getCountOfPositionPairs();
+    let skip = 4000;//await atlasService.getCountOfPositionPairs();
     // skip = Number.parseInt(skip);
     const batchSize = 200;
 
