9b5846a8bf741f654d4f07ce4e5ae7aff128917f
Thanasis Galatis <gnasos219@gmail.com>
2025-06-15 22:38:43 +0300

    OPS - Added one retry because maximum retry attempts were reached for an entry sheet

--- backend/controllers/OPSController.js
+++ backend/controllers/OPSController.js
@@ -68,7 +68,7 @@ const sendDeltioEisodouWS = async (req, res) => {
 
     let responseCall2;
     try {
-      const MAX_RETRIES = 3;
+      const MAX_RETRIES = 4;
       const RETRY_DELAY = 5000; // 5 seconds
 
       responseCall2 = await callServiceWithRetry(soapUrl, xmlPostStringCall2, MAX_RETRIES, RETRY_DELAY, soapActionCall2);
